ba1c5fb8efa82a78fd2bb05566dc84a4f3c0d845
Humberto Tejada <humtej1204@gmail.com>
2023-12-08 04:14:52 -0500

    [Update]: Update next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
 
+const isProd = process.env.NODE_ENV = 'production'
+
 const nextConfig = {
+    // basePath: isProd ? '/NextJs-Portafolio_Page' : '',
     sassOptions: {
       includePaths: [path.join(__dirname, 'styles')],
     },
     output: "export",
     distDir: "dist",
     images: {
-      unoptimized: true
+      unoptimized: true,
+      domains: ['localhost', 'humtej1204.github.io/NextJs-Portafolio_Page'],
     }
 }
 
