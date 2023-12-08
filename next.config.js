cb647dae85935fda6a8e52c3c90fcb4a1ac043b3
Humberto Tejada Espinoza <90359858+humtej1204@users.noreply.github.com>
2023-12-08 03:34:44 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
     sassOptions: {
       includePaths: [path.join(__dirname, 'styles')],
     },
+    exportPathMap: function () { // /Next-React-Components
+        return {
+          "/": { page: "/" },
+          "/ap-grid-layout": { page: "/ap-grid-layout" },
+          "/ap-highlight": { page: "/ap-highlight" },
+        }
+    },
 }
 
 module.exports = nextConfig
