34fdc4aa6562fa4b8dc096def7266ab0c22bdb3c
Humberto Tejada <humtej1204@gmail.com>
2023-12-09 19:31:32 -0500

    [Update]: Add basePath in next.config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const path = require('path')
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
+    basePath: isProd ? '/NextJs-Portafolio_Page' : undefined,
     assetPrefix: isProd ? 'https://humtej1204.github.io/NextJs-Portafolio_Page/' : undefined,
     sassOptions: {
       includePaths: [path.join(__dirname, 'styles')],
